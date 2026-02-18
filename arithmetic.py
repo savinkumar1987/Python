6a634fabebb65cd1830d03d5c2c77228cf09106f
savinkumar1987 <savinkumar1987@gmail.com>
2026-02-18 22:50:12 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,10 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
